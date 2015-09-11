0136f8f50b4016db49342c11230a32b64c375440
Rajib Chowdhury <chowdhury284@gmail.com>
2015-09-11 18:00:40 +0530

    Initial commit.

---
+++ routes/demasonis.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET demasonis listing. */
+router.post('/', function(req, res, next) {
+	var req_body = req.body
+	
+	res.status(201)
+	res.send({ github_callback_body: req_body });
+});
+
+module.exports = router;
